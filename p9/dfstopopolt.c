7afb99130e846f9c8deee6ae3c3916d4a4904f96
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2023-07-27 19:43:56 +0530

    Update dfstopopolt.c

--- p9/dfstopopolt.c
+++ p9/dfstopopolt.c
@@ -1,3 +1,26 @@
+/*program to implement the topological sorting with dfs on adjacency matrix*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 100
+int graph[MAX][MAX], visited[MAX],path[MAX], count=0;
+int stack[MAX], top=-1;
+int c=0;
+
+void dfs(int n, int start) {
+    visited[start] = 1;
+    path[start] =1;
+    for(int i=0; i<n; i++)
+      {
+          if(graph[start][i] && visited[i]==1&& path[i]==1)
+              c=1 ;
+        if(graph[start][i] && visited[i]==0)
+            dfs(n, i);
+       }
+       path[start]=0;
+    stack[++top] = start;
+}
+
 void ploter(int k)
 {
 
