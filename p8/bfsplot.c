88e53fbdcaac3dd5ad81d6df880763fe97595cc4
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2023-07-29 12:49:44 +0530

    Update bfsplot.c

--- p8/bfsplot.c
+++ p8/bfsplot.c
@@ -41,9 +41,6 @@ void ploter(int k)
     for(int i=1;i<=10;i++)
   {
     v=i;
-  int *arr;
-  for(int i=0;i<v;i++)
-  arr[i]=(int *)malloc(sizeof(int)*v); 
 
 
  if(k==0)
@@ -56,10 +53,10 @@ void ploter(int k)
        
        if(i!=j)
        {
-        arr[i][j] =1;
+        graph[i][j] =1;
        }
        else
-       arr[i][j] =0;
+       graph[i][j] =0;
   }
 }
  }
@@ -69,11 +66,11 @@ if(k==1)
     for(int i=0;i<v;i++)
     {
         for(int j=0;j<v;j++)
-         arr[i][j] =0;
+        graph[i][j] =0;
     }
     for(int i=0;i<v-1;i++)
     {
-            arr[i][i+1]=1;
+            graph[i][i+1]=1;
     }
 
 }
@@ -98,8 +95,6 @@ if(k==1)
          else
           fprintf(f1,"%d\t%d\n",v,orderCount);
          // printf("%d\t%d\n",v,orderCount);
-
-    free(arr);
   }
 
   fclose(f1);
