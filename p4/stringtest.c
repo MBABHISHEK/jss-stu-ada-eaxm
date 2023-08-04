acd1b3954defa5c36ca702ae13055e395f802d4b
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2023-08-04 13:27:25 +0530

    Update stringtest.c

--- p4/stringtest.c
+++ p4/stringtest.c
@@ -1,25 +1,22 @@
-//Program to perform analysis of brute force string matching
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 int count = 0;
 
 int stringmatching(char *text, char *pattern, int n, int m) {
     count = 0;
-    for(int i=0; i<=n-m; i++)
-     {
-        int j=0;
-        while(j<m)
-         {
+    for (int i = 0; i <= n - m; i++) {
+        int j = 0;
+        while (j < m) {
             count++;
-            if(pattern[j] != text[i+j]) 
+            if (pattern[j] != text[i + j])
                 break;
             j++;
         }
-        if(j==m) {
+        if (j == m) {
             printf("THE PATTERN FOUND \n");
-
             return count;
         }
     }
@@ -27,22 +24,28 @@ int stringmatching(char *text, char *pattern, int n, int m) {
     return count;
 }
 
-void main()
-{
-  
-    int m,n;
-    char text[100],pattern[100];
+int main() {
+    int m, n;
+    char text[100], pattern[100];
+
     printf("ENTER THE PATTERN LENGTH\n");
-    scanf("%d",&m);
-    printf("ENTER THE TEXT LENGTH\n");
-    scanf("%d",&n);
-    printf("ENTER THE TEXT\n");
-    scanf("%s",text);
+    scanf("%d", &m);
     printf("ENTER THE PATTERN\n");
-    scanf("%s",pattern);
-    
-            stringmatching(text, pattern, n,m); 
+    getchar(); // Consume the newline character left in the input buffer
 
+    fgets(pattern, sizeof(pattern), stdin);
+    pattern[strcspn(pattern, "\n")] = '\0'; // Remove the newline character from the input
+
+    printf("ENTER THE TEXT LENGTH\n");
+    scanf("%d", &n);
+    printf("ENTER THE TEXT\n");
+    getchar(); // Consume the newline character left in the input buffer
+
+    fgets(text, sizeof(text), stdin);
+    text[strcspn(text, "\n")] = '\0'; // Remove the newline character from the input
+
+    int comparisons = stringmatching(text, pattern, n, m);
+    printf("Number of comparisons: %d\n", comparisons);
+
+    return 0;
 }
-
-
