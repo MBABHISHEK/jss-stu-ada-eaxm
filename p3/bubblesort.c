57baff2e91d5b303a9115b658fc039cc2d59dffa
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2024-09-01 17:07:11 +0530

    Update bubblesort.c

--- p3/bubblesort.c
+++ p3/bubblesort.c
@@ -1,125 +1,120 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int count;
+int bubblesort(int *a, int n) {
+    int count = 0;
+    int i, j, t, flag;
 
-
-int bubblesort(int *a,int n)
-{
-    count = 0;
-     int i,j,t,flag=0;
-   for(i=0;i<n-1;i++)
-   {
-      flag=0;
-    for(j=0;j<n-i-1;j++)    
-     {
-         count++;    
-        if(a[j]>a[j+1])
-      {
-         t=*(a+j);
-         *(a+j)=*(a+j+1);
-         *(a+j+1)=t;
-         flag=1;
-      }  
-     }
-    
-      if(flag==0)
-      break;
-      }
-
-   return count;
+    for (i = 0; i < n - 1; i++) {
+        flag = 0;
+        for (j = 0; j < n - i - 1; j++) {
+            count++;
+            if (a[j] > a[j + 1]) {
+                t = *(a + j);
+                *(a + j) = *(a + j + 1);
+                *(a + j + 1) = t;
+                flag = 1;
+            }
+        }
+        if (flag == 0) {
+            break;
+        }
+    }
+    return count;
 }
 
+void plotter() {
+    int *arr, n;
+    FILE *f1, *f2, *f3;
 
-void plotter()
-{
- 
-   int *arr,n;
-   srand(time(NULL));
-   FILE *f1,*f2,*f3;
- 
-   f1=fopen("BUBBLWBEST.txt","a");
-   f2=fopen("BUBBLEWORST.txt","a");
-   f3=fopen("BUBBLEAVG.txt","a");
-    n=10;
-   
-    while(n<=30000)
-    {
+    f1 = fopen("BUBBLEBEST.txt", "a");
+    f2 = fopen("BUBBLEWORST.txt", "a");
+    f3 = fopen("BUBBLEAVG.txt", "a");
+    n = 10;
 
-       arr=(int *)malloc(sizeof(int)*n);
-      for(int i=0;i<n;i++)
-      *(arr+i)=n-i;
-       count=0;
-        //wrost case
-        bubblesort(arr,n);
-        fprintf(f2,"%d\t%d\n",n,count);
-        //printf("%d\t%d\n",n,count);
- 
-     //best case
-      count=0;
-      for(int i=0;i<n;i++)
-      *(arr+i)=i+1;
-      bubblesort(arr,n);
-      fprintf(f1,"%d\t%d\n",n,count);
-      //printf("%d\t%d\n",n,count);
+    while (n <= 30000) {
+        arr = (int *)malloc(sizeof(int) * n);
 
-     //AVG case
-      for(int i=0;i<n;i++)
-      *(arr+i)=rand()%n;
-      count=0;
-      bubblesort(arr,n);
-      fprintf(f3,"%d\t%d\n",n,count);
-      if(n<10000)
-      n=n*10;
-      else
-      n=n+10000;
-      free(arr);
+        // Worst case
+        for (int i = 0; i < n; i++) {
+            *(arr + i) = n - i;
+        }
+        int count = bubblesort(arr, n);
+        fprintf(f2, "%d\t%d\n", n, count);
+
+        // Best case
+        for (int i = 0; i < n; i++) {
+            *(arr + i) = i + 1;
+        }
+        count = bubblesort(arr, n);
+        fprintf(f1, "%d\t%d\n", n, count);
+
+        // Average case
+        for (int i = 0; i < n; i++) {
+            *(arr + i) = rand() % n;
+        }
+        count = bubblesort(arr, n);
+        fprintf(f3, "%d\t%d\n", n, count);
+
+        if (n < 10000) {
+            n *= 10;
+        } else {
+            n += 10000;
+        }
+        free(arr);
     }
+
     fclose(f1);
     fclose(f2);
     fclose(f3);
 }
 
+void tester() {
+    int *arr, n;
+    printf("ENTER THE NUMBER OF ELEMENTS\n");
+    scanf("%d", &n);
 
-void tester()
-{
-  int *arr, n;
-   printf("ENTER THE NUMBER OF ELEMENTS\n");
-   scanf("%d",&n);
+    arr = (int *)malloc(sizeof(int) * n);
+    printf("ENTER THE ELEMENTS OF THE ARRAY\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
 
-   arr=(int *)malloc(sizeof(int)*n);
-   printf("ENTER THE ELEMENTS OF THE ARRAY\n");
-      for(int i=0;i<n;i++)
-       scanf("%d",&arr[i]);
-
-  printf("THE ELEMENTS OF THE ARRAY BEFORE SORTING\n"); 
-    for(int i=0;i<n;i++)
-       printf("%d ",arr[i]);
+    printf("THE ELEMENTS OF THE ARRAY BEFORE SORTING\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
     printf("\n");
 
-      bubblesort(arr,n);
+    bubblesort(arr, n);
 
-      printf("THE ELEMENTS OF THE ARRAY BEFORE SORTING\n"); 
-    for(int i=0;i<n;i++)
-       printf("%d ",arr[i]);
-       printf("\n");
-       printf("\n");
+    printf("THE ELEMENTS OF THE ARRAY AFTER SORTING\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    free(arr);
 }
 
-
-void main()
-{
-    for(;;)
-    {
+int main() {  // Changed to int and added return 0
+    for (;;) {
         int key;
-        printf("ENTER THE CHOICE \n1.TO TEST \n2.TO PLOT\nO  TO EXIT\n");
-        scanf("%d",&key);
-         
-         switch(key)
-         {
-           case 1:tester();break;
-           case 2:plotter();break;
-           default:exit(1);
-         } 
+        printf("ENTER THE CHOICE \n1.TO TEST \n2.TO PLOT\n0 TO EXIT\n");
+        scanf("%d", &key);
+
+        switch (key) {
+            case 1:
+                tester();
+                break;
+            case 2:
+                plotter();
+                break;
+            case 0:
+                return 0;
+            default:
+                printf("Invalid choice, try again.\n");
+        }
     }
+    return 0;
 }
