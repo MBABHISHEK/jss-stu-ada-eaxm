7d2ec0cd7948fdbddb4d80795aafd57035ea6fd7
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2024-09-01 21:52:10 +0530

    Update memoryplot.c

--- p13/memoryplot.c
+++ p13/memoryplot.c
@@ -5,27 +5,40 @@ int max(int a, int b) {
     return (a > b) ? a : b;
 }
 
-int t[100][100], v[100], w[100];
+int t[100][100], v[100], w[100], n, m, i, j;
 int operationCount = 0; // To count the number of operations
 
-void knapsack(int n, int m) {
-    int i, j;
-    for (i = 0; i < n + 1; i++) {
-        for (j = 0; j < m + 1; j++) {
-            operationCount++; // Incrementing operation count for each iteration
-            if (i == 0 || j == 0)
-                t[i][j] = 0;
-            else if (j < w[i])
-                t[i][j] = t[i - 1][j];
-            else
-                t[i][j] = max(t[i - 1][j], v[i] + t[i - 1][j - w[i]]);
+int knap(int i, int j) {
+    operationCount++; // Incrementing operation count for each call
+
+    if (t[i][j] == -1) {
+        if (j < w[i])
+            t[i][j] = knap(i - 1, j);
+        else
+            t[i][j] = max(knap(i - 1, j), v[i] + knap(i - 1, j - w[i]));
+    }
+    return t[i][j];
+}
+
+void initializeMemoizationTable() {
+    for (i = 0; i <= n; i++) {
+        for (j = 0; j <= m; j++) {
+            t[i][j] = -1;
         }
     }
 
-    printf("THE MAXIMUM VALUE IS : %d\n", t[n][m]);
+    // Initialize the base cases
+    for (i = 0; i <= n; i++) {
+        t[i][0] = 0;
+    }
+    for (j = 0; j <= m; j++) {
+        t[0][j] = 0;
+    }
+}
 
+void printComposition(int n, int m) {
     printf("THE COMPOSITION IS \n");
-    for (i = n; i > 0; i--) {
+    for (int i = n; i > 0; i--) {
         if (t[i][m] != t[i - 1][m]) {
             printf("%d ", i);
             m = m - w[i];
@@ -37,7 +50,6 @@ void knapsack(int n, int m) {
 void main() {
     FILE *f1;
     f1 = fopen("knapsackgraph.txt", "a");
-    int n, m, i;
 
     printf("ENTER NO OF ITEMS:\n");
     scanf("%d", &n);
@@ -45,12 +57,19 @@ void main() {
     scanf("%d", &m);
 
     printf("WEIGHT\tVALUE\n");
-    for (i = 1; i < n + 1; i++) {
+    for (i = 1; i <= n; i++) {
         scanf("%d\t%d", &w[i], &v[i]);
     }
 
     operationCount = 0; // Resetting the operation count before the function call
-    knapsack(n, m);
+
+    // Initialize the memoization table
+    initializeMemoizationTable();
+
+    int maxValue = knap(n, m);
+
+    printf("THE MAXIMUM VALUE IS : %d\n", maxValue);
+    printComposition(n, m);
 
     printf("Operation Count: %d\n", operationCount);
     fprintf(f1, "Items: %d, Capacity: %d, Operation Count: %d\n", n, m, operationCount);
